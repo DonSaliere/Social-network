fec0848d01e72bfd3400bd2c7747f9011e0d2cd7
DonSaliere <s.andrey-klg@yandex.ru>
2019-11-11 21:53:41 +0300

    удаление комментариев

--- src/index.js
+++ src/index.js
@@ -7,21 +7,11 @@ import App from './App';
 import {Provider} from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-
-
     ReactDOM.render(
         <BrowserRouter>
             <Provider store={store}>
                 <App />
             </Provider>
         </BrowserRouter>,document.getElementById('root'));
-
-
-//rerenderEntireTree(store.getState());
-//
-//store.subscribe(() => {
-//    let state = store.getState();
-//    rerenderEntireTree(state);
-//});
 
 serviceWorker.unregister();
