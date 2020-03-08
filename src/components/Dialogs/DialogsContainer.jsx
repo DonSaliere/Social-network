00fb40dacabc930d7feb2f252f59cc07913bfd92
DonSaliere <s.andrey-klg@yandex.ru>
2020-03-09 00:08:58 +0300

    69 - React JS - High Order Component (hoc)

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { updateNewMessageBodyCreator, sendMessageCreator } from '../Redux/dialogs-reducer';
 import Dialogs from './Dialogs';
 import { connect } from 'react-redux';
+import { withAuthRedirect } from '../../hoc/withAuthRedirect';
 
 
 let mapsStateToProps = (state) => {
     return {
-        dialogsPage: state.dialogsPage,
-        isAuth: state.auth.isAuth
+        dialogsPage: state.dialogsPage
     }
 }
 
@@ -23,6 +23,8 @@ let mapsDispatchToProps = (dispatch) => {
     }
 }
 
-const DialogsContainer = connect(mapsStateToProps, mapsDispatchToProps)(Dialogs);
+let AuthRedirectComponent = withAuthRedirect(Dialogs);
+
+const DialogsContainer = connect(mapsStateToProps, mapsDispatchToProps)(AuthRedirectComponent);
 
 export default DialogsContainer;
