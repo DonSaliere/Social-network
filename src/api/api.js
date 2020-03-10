6d8a08ccdd161895c45b28a77382471f7be91da0
DonSaliere <s.andrey-klg@yandex.ru>
2020-03-10 19:01:57 +0300

    добавление пропущенных файлов

---
+++ src/api/api.js
@@ -0,0 +1,35 @@
+import * as axios from 'axios';
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: { "API-KEY": "a17e9430-5c80-41d5-b26b-8cfa074fdfe6" }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    },
+    follow(userId) {
+        return instance.post(`follow/${userId}`)
+    },
+    unfollow(userId) {
+        return instance.delete(`follow/${userId}`)
+    },
+    getProfile(userId) {
+        return instance.get(`profile/` + userId)
+    }
+}
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`)
+    }
+}
+
+
+
