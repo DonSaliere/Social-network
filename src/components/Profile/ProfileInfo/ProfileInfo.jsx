6988c5898bc203456f19f248547230ab81f9b733
DonSaliere <s.andrey-klg@yandex.ru>
2020-03-24 14:40:08 +0300

    71 - React JS - setState, local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
 
@@ -10,12 +11,12 @@ const ProfileInfo = (props) => {
 
   return (
     <div>
-      <div>
+      {/*<div>
         <img className={s.img} src='https://s1.best-wallpaper.net/wallpaper/m/1110/Beautiful-dream-beach_m.jpg'></img>
-      </div>
+      </div>*/}
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large}/>     
-        ava
+        <ProfileStatus status ={"TEST"}/>
       </div>
       <div className={s.descriptionBlock}>{props.profile.contacts.vk}</div>
     </div>
